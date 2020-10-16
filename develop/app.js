c34c1652474bb6474b8b1f95343b72d9b6a5600d
Giovanny Franco <gfranco19@live.com>
2020-10-15 22:10:38 -0700

    added create engineer and create intern

--- develop/app.js
+++ develop/app.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/manager");
-const Engineer = require("./lib/Engineer");
+const Engineer = require("./lib/engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -47,7 +47,7 @@ function createMgmt (){
             const manager = new Manager(response.name, response.id, response.email, response.office);
             team.push(manager);
             console.log(manager);
-            CreateEmployee();
+            createEmployee();
         });
 
     }else{
@@ -59,9 +59,84 @@ function createMgmt (){
 
 }
 
-function CreateEmployee(){
-    
+function createEmployee(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "What type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "Return"],
+            name:"employeeNew"
+        }
+    ]).then(function(response){
+        if(response.employeeNew === "Engineer"){
+            createEngineer();
+        }else if(response.employeeNew === "Intern"){
+            createIntern();
+        }else{
+            console.log("Stopped creating new employee.");
+            render(team);
+        }
+    });
 }
 
+function createEngineer(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your new Engineer's Name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is your new Engineer's id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your new Engineer's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your new Engineer's github?",
+            name: "github"
+        }
+    ]).then(function(response){
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        team.push(engineer);
+        console.log(engineer);
+        createEmployee();
+    });
+}
+
+function createIntern(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your new Intern's Name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is your new Intern's id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your new Intern's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What school is your new Intern currently enrroled in?",
+            name: "school"
+        }
+    ]).then(function(response){
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        team.push(intern);
+        console.log(intern);
+        createEmployee();
+    });
+}
 
 createMgmt();
